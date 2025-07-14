adfe47d615870b07c705deb766f7bbf172956388
Roel de Jong <Roel4811@hotmail.com>
2025-07-14 12:33:33 +0200

    add return shape

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import express from "express"
+import data from "./data/player_data_v1.json"
+import { Player } from "./types"
+import { findPlayer, findPlayerMatches } from "./lib/players"
+import { calcTotalSalary } from "./lib/salary"
 
 const app = express()
 const port = 3000
 
 app.get("/salary/player/:id", (_req, res) => {
   const id = _req.params.id
-  res.json({ id })
+
+  const player = findPlayer(data.players, Number(id))
+  const playerMatches = findPlayerMatches(player.id, data.matches)
+  res.json({
+    id,
+    name: player.name,
+    totalSalary: calcTotalSalary(player, playerMatches),
+  })
 })
 
 app.listen(port, () => {
